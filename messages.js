ca1f596cabf04249fa11b1aa92bfe3eb6937d9c2
Cameron Mullins <cmullins510@gmail.com>
2019-10-30 10:45:35 +0800

    added messages.js file

---
+++ messages.js
@@ -0,0 +1,159 @@
+const mockAPI = {
+    "myInfo": {
+        "myName": "Cameron Mutts",
+        "icon": "pup1.jpg",
+        "number": "+1 530-386-7350"
+    },
+    "defaulIcon": "defaultIcon.svg",
+    "conversations":[
+        {
+            "lastMessage": "Sun Oct 27 2019 11:37:00 GMT+0800",
+            "isGroup": false,
+            "senderName": 'Travis Barker',
+            "senderIcon": "pup2.jpg",
+            "messages": [
+                {
+                    "sent": false,
+                    "sender": {
+                        "name": "Travis Barker",
+                        "icon": "pup2.jpg",
+                        "number": "+1 415-867-5309"
+                    },
+                    "message": "Fixie lomo tilde, blog neutra poutine kale chips pabst iPhone adaptogen subway tile kickstarter.",
+                    "timeSent": "Sun Oct 27 2019 11: 37: 00 GMT+0800"
+                },
+                {
+                    "sent": true,
+                    "message": "Fashion axe woke knausgaard tumeric aesthetic mumblecore drinking vinegar mlkshk jianbing. Hashtag four dollar toast pork belly seitan.",
+                    "timeSent": "Sun Oct 27 2019 11: 35: 00 GMT+0800"
+                },
+                {
+                    "sent": false,
+                    "sender": {
+                        "name": "Travis Barker",
+                        "icon": "pup2.jpg",
+                        "number": "+1 415-867-5309"
+                    },
+                    "message": "Brooklyn paleo migas kogi, salvia tofu deep v chia drinking vinegar affogato",
+                    "timeSent": "Sun Oct 27 2019 11:34:00 GMT+0800"
+                },
+                {
+                    "sent": false,
+                    "sender": {
+                        "name": "Travis Barker",
+                        "icon": "pup2.jpg",
+                        "number": "+1 415-867-5309"
+                    },
+                    "message": "Crucifix pickled subway tile authentic fingerstache. Biodiesel ennui actually, listicle copper mug craft beer master cleanse.",
+                    "timeSent": "Sun Oct 27 2019 11:33:30 GMT+0800"
+                },
+                {
+                    "sent": true,
+                    "message": "Llisticle Brooklyn paleo migas kogi, salvia tofu deep v chia drinking vinegar affogato",
+                    "timeSent": "Sun Oct 27 2019 11:33:00 GMT+0800"
+                }
+            ]
+        },
+        {
+            "lastMessage": "Sun Oct 26 2019 09:37:00 GMT+0800",
+            "isGroup": true,
+            "senderName": "Barky Bark and the Puppy Bunch",
+            "senderIcon": 'barkyBark.jpg',
+            "messages": [
+                {
+                    "sent": false,
+                    "sender": {
+                        "name": "Bark Walhberg",
+                        "icon": "pup3.png",
+                        "number": "+1 714-489-4608"
+                    },
+                    "message": "Direct trade artisan lo-fi vaporware tofu. Poutine before they sold out coloring book glossier.",
+                    "timeSent": "Sun Oct 26 2019 09:37:00 GMT+0800"
+                },
+                {
+                    "sent": true,
+                    "message": "Sriracha plaid skateboard cloud bread art party four loko. Poutine live-edge tumblr, listicle literally bitters yr selvage twee stumptown salvia.",
+                    "timeSent": "Sun Oct 26 2019 09:35:00 GMT+0800"
+                },
+                {
+                    "sent": false,
+                    "sender": {
+                        "name": "Hector Barkos",
+                        "icon": "pup4.jpg",
+                        "number": "+1 415-867-5309"
+                    },
+                    "message": "Brooklyn paleo migas kogi, salvia tofu deep v chia drinking vinegar affogato",
+                    "timeSent": "Sun Oct 26 2019 09:34:00 GMT+0800"
+                },
+                {
+                    "sent": false,
+                    "sender": {
+                        "name": "Terry Yipcey",
+                        "icon": "pup5.png",
+                        "number": "+1 415-867-5309"
+                    },
+                    "message": "Crucifix pickled subway tile authentic fingerstache. Biodiesel ennui actually, listicle copper mug craft beer master cleanse.",
+                    "timeSent": "Sun Oct 26 2019 09:33:30 GMT+0800"
+                },
+                {
+                    "sent": false,
+                    "sender": {
+                        "name": "Scott Paws",
+                        "icon": "pup6.png",
+                        "number": "+1 415-867-5309"
+                    },
+                    "message": "Llisticle Brooklyn paleo migas kogi, salvia tofu deep v chia drinking vinegar affogato",
+                    "timeSent": "Sun Oct 26 2019 09:33:00 GMT+0800"
+                }
+            ]
+        },
+        {
+            "lastMessage": "Sun Oct 25 2019 22:37:00 GMT+0800",
+            "isGroup": false,
+            "senderName": 'Sir Barks Alot',
+            "senderIcon": "sirBarksAlot.jpeg",
+            "messages": [
+                {
+                    "sent": false,
+                    "sender": {
+                        "name": "Sir Barks Alot",
+                        "icon": "sirBarksAlot.jpeg",
+                        "number": "+1 415-867-5309"
+                    },
+                    "message": "Tacos portland yr readymade lyft whatever gochujang green juice four dollar toast brunch etsy williamsburg. Bitters 90's hell of copper mug tumeric try-hard whatever pinterest swag banjo occupy af",
+                    "timeSent": "Sun Oct 25 2019 22:37:00 GMT+0800"
+                },
+                {
+                    "sent": true,
+                    "message": "Tattooed vice farm-to-table flannel DIY small batch",
+                    "timeSent": "Sun Oct 25 2019 22:35:00 GMT+0800"
+                },
+                {
+                    "sent": false,
+                    "sender": {
+                        "name": "Sir Barks Alot",
+                        "icon": "sirBarksAlot.jpeg",
+                        "number": "+1 415-867-5309"
+                    },
+                    "message": "Sriracha offal bushwick etsy meh gochujang humblebrag hoodie brunch godard crucifix microdosing. Palo santo af narwhal, fixie migas affogato tofu",
+                    "timeSent": "Sun Oct 25 2019 22:34:00 GMT+0800"
+                },
+                {
+                    "sent": false,
+                    "sender": {
+                        "name": "Sir Barks Alot",
+                        "icon": "sirBarksAlot.jpeg",
+                        "number": "+1 415-867-5309"
+                    },
+                    "message": "Tumeric tilde salvia migas snackwave food truck. Biodiesel tilde waistcoat master cleanse mixtape.",
+                    "timeSent": "Sun Oct 25 2019 22:33:30 GMT+0800"
+                },
+                {
+                    "sent": true,
+                    "message": "Shoreditch four loko four dollar toast, you probably haven't heard of them beard asymmetrical swag mumblecore franzen austin tofu kickstarter. Vegan iPhone art party gentrify kinfolk, williamsburg schlitz typewriter lomo hella DIY seitan hashtag trust fund bicycle rights. Sriracha offal bushwick etsy meh gochujang humblebrag hoodie brunch godard crucifix microdosing. Palo santo af narwhal, fixie migas affogato tofu. Llisticle Brooklyn paleo migas kogi, salvia tofu deep v chia drinking vinegar affogato",
+                    "timeSent": "Sun Oct 25 2019 22:33:00 GMT+0800"
+                }
+            ]
+        }
+    ]
+}
